7d7679e6cec0f19896d183a42d940f5bc1ac4fd2
smythp <patricksmyth01@gmail.com>
2016-02-06 15:09:07 -0500

    Added <meta> tag for utf-8

--- pull.py
+++ pull.py
@@ -1,7 +1,7 @@
 import requests
 from readability.readability import Document
 
-url_in = "https://torrentfreak.com/paypal-starts-banning-vpn-and-smartdns-services-160205/"
+url_in = "" # put url here
 
 r = requests.get(url_in)
 # print(r.status_code)
@@ -11,6 +11,5 @@ html = r.text
 with open('out.html','w') as out_file:
     readable_article = Document(html).summary()
     readable_article = readable_article.replace(u"\u2018", "'").replace(u"\u2019", "'").replace(u"\u201c","\"").replace(u"\u201d", "\"")
-    out_file.write(readable_article)
-
-
+    out_file.write("<!DOCTYPE html>\n<html><head><meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" /></head>")
+    out_file.write(readable_article[6:])
